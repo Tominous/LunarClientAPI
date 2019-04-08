a51f5644aba11e4f515623a3f75546ce2b3dbbd4
AgentManny <itsreallymanny@gmail.com>
2019-04-08 15:57:45 +0100

    removed quantum dependency

--- src/main/java/gg/manny/lunar/LunarClientAPI.java
+++ src/main/java/gg/manny/lunar/LunarClientAPI.java
@@ -1,12 +1,14 @@
 package gg.manny.lunar;
 
-import gg.manny.lunar.command.TestCommand;
+import gg.manny.lunar.command.CheckCommand;
 import gg.manny.lunar.listener.PlayerListener;
-import gg.manny.quantum.Quantum;
 import lombok.Getter;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandMap;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -23,14 +25,31 @@ public class LunarClientAPI extends JavaPlugin {
     public void onEnable() {
         instance = this;
 
-        Quantum quantum = new Quantum(this);
-        quantum.registerCommand(new TestCommand());
+        this.registerCommand(new CheckCommand(this));
 
         this.getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
     }
 
     public static boolean onClient(Player player) {
         return players.contains(player.getUniqueId());
+    }
+
+    private void registerCommand(Command command) {
+        Field bukkitCommandMap = null;
+        try {
+            bukkitCommandMap = this.getServer().getClass().getDeclaredField("commandMap");
+            bukkitCommandMap.setAccessible(true);
+        } catch (NoSuchFieldException | SecurityException e) {
+            e.printStackTrace();
+        }
+
+        CommandMap commandMap;
+        try {
+            commandMap = (org.bukkit.command.CommandMap) bukkitCommandMap.get(this.getServer());
+            commandMap.register(this.getDescription().getName(), command);
+        } catch (IllegalArgumentException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 
 }
